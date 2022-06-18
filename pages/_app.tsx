692a22e39e8629e860e7f2c18b1407ceb61f3886
ryoya <ryoya.miyamoto226@gmail.com>
2022-06-18 16:37:28 +0900

    add qrcode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// _app/index.tsx
+import { ChakraProvider } from "@chakra-ui/react";
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
-
-export default MyApp
