ed53041cf5c7aeada0949ecf91b916be609a26f3
miyaRyo226 <miyaRyo226@users.noreply.github.com>
2022-06-21 11:37:56 +0000

    deploy: e0d0ff30ce5549a37c424f523242a8f01e99192b

---
+++ _next/static/q_75pTdVT9VLF8SxUe5Db/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75-073206bcfd825370.js","static/chunks/pages/index-babcb7f74c182590.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
